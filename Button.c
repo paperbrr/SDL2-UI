b3eb6131e71c106bdf471b1207e83a0bb478aac1
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-28 18:00:27 +0530

    added functionality for custom button colors

--- Button.c
+++ Button.c
@@ -14,13 +14,16 @@ void buttonsArr_init(ButtonsArray* buttonArr, int initialSize){
     buttonArr->length = 0;
 };
 
-void createButton(ButtonsArray* buttonArr, int x, int y, int h, int w, void (*action) ()){
+void createButton(ButtonsArray* buttonArr, int x, int y, int h, int w, void (*action) (), SDL_Color color){
     Button* newButton = malloc(sizeof(Button));
+
     newButton->sourceRect.x = x;
     newButton->sourceRect.y = y;
     newButton->sourceRect.h = h;
     newButton -> sourceRect.w = w;
+
     newButton->actionFunc = action;
+    newButton->color = color;
 
     buttonArr->buttons = realloc(buttonArr->buttons, buttonArr->arrDataSize+sizeof(newButton));
     buttonArr->buttons[buttonArr->length] = newButton;
@@ -41,8 +44,9 @@ void buttonClickHandler(SDL_Event* event ,ButtonsArray* buttonArr){
 }
 
 void renderButtons(ButtonsArray* buttonArr, SDL_Renderer* renderer){
-    SDL_SetRenderDrawColor(renderer, 0, 0, 180, 255);
     for (int i=0; i<buttonArr->length; i++){
+        SDL_Color renderColor = buttonArr->buttons[i]->color;
+        SDL_SetRenderDrawColor(renderer, renderColor.r, renderColor.g, renderColor.b, renderColor.a);
         SDL_RenderFillRect(renderer, &buttonArr->buttons[i]->sourceRect);
     }
 }
