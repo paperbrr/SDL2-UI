be05a80f321f57fcbf9186a8596005b444c74d30
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-12 10:43:13 +0530

    rendering text now uses specified color instead of default

--- UIElements/Utils.c
+++ UIElements/Utils.c
@@ -6,7 +6,7 @@
 char* fontPath = NULL;
 int fontSize = 0;
 
-SDL_Texture* createCharTexture(char* text, int* wrapperW, int* wrapperH, SDL_Renderer* renderer){
+SDL_Texture* createCharTexture(char* text, int* wrapperW, int* wrapperH, SDL_Renderer* renderer, SDL_Color textColor){
     if (fontPath==NULL || fontSize==0){printf("Font path/size not initialised. Use fontInit() to proceed."); exit(1);}
     TTF_Font* font = TTF_OpenFont(fontPath, 25);
     if (!font){
@@ -17,7 +17,7 @@ SDL_Texture* createCharTexture(char* text, int* wrapperW, int* wrapperH, SDL_Ren
     if (wrapperW && wrapperH) {TTF_SizeText(font, text, wrapperW, wrapperH);}
     // ^^for the wrapper, if needed :D
 
-    SDL_Color textColor = {0, 190, 190, 255};
+    //SDL_Color textColor = {0, 190, 190, 255};
     SDL_Surface* charSurface = TTF_RenderText_Blended(font, text, textColor);
     SDL_Texture* charTexture = SDL_CreateTextureFromSurface(renderer, charSurface);
 
